d26a119734014580e856af6955b321716292adae
anand-supernova <anand.tripathy@supernova.xyz>
2018-06-14 09:43:53 +0530

    Fix to show backend payment method option

--- app/controllers/spree/admin/payments_controller_decorator.rb
+++ app/controllers/spree/admin/payments_controller_decorator.rb
@@ -2,14 +2,14 @@ Spree::Admin::PaymentsController.class_eval do
 
   private
 
-    def load_data
-      @amount = params[:amount] || load_order.total
-      @payment_methods = available_payment_methods
-      @payment_method = @payment.try(:payment_method) || @payment_methods.first
-    end
+    # def load_data
+    #   @amount = params[:amount] || load_order.total
+    #   @payment_methods = available_payment_methods
+    #   @payment_method = @payment.try(:payment_method) || @payment_methods.first
+    # end
 
-    def available_payment_methods
-      @order.subscriptions.any? ? Spree::Gateway.active.available(:backend) : Spree::PaymentMethod.available(:backend)
-    end
+    # def available_payment_methods
+    #   @order.subscriptions.any? ? Spree::Gateway.active.available(:backend) : Spree::PaymentMethod.available(:backend)
+    # end
 
 end
