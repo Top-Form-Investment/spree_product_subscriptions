7e8fa8e7d0e6eceadd0147e2bb9cddc06b34d878
anand-supernova <anand.tripathy@supernova.xyz>
2018-07-25 11:56:04 +0530

    Changes for prior notify condition

--- app/models/spree/subscription.rb
+++ app/models/spree/subscription.rb
@@ -34,7 +34,7 @@ module Spree
     scope :active, -> { where(enabled: true) }
     scope :not_cancelled, -> { where(cancelled_at: nil) }
     scope :with_appropriate_delivery_time, -> { where("next_occurrence_at <= :current_date", current_date: Time.current) }
-    scope :with_future_delivery_time, -> { where("next_occurrence_at >= :current_date", current_date: Time.current) }
+    scope :with_future_delivery_time, -> { where("DATE(next_occurrence_at) >= ?", Date.today) }
     scope :eligible_for_subscription, -> { unpaused.active.not_cancelled.with_appropriate_delivery_time }
     scope :eligible_for_prior_notification, -> { unpaused.active.not_cancelled.with_future_delivery_time }
 
@@ -199,7 +199,7 @@ module Spree
       def set_for_next_retry
         if failed_attempts < 3
           self.failed_attempts += 1
-          self.next_occurrence_at = next_occurrence_at + 7.days
+          self.next_occurrence_at = self.next_occurrence_at + 7.days
           self.save
           if self.failed_attempts == 3
             cancel
@@ -230,7 +230,7 @@ module Spree
 
       def add_payment_method_to_order(order)
         if order.payments.exists?
-          order.payments.first.update(source: source, payment_method: source.payment_method)
+          order.payments.last.update(source: source, payment_method: source.payment_method)
         else
           order.payments.create(amount: order.total, source: source, payment_method: source.payment_method)
         end
