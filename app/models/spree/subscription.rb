cbfc704ad39f90c4e627b358ac48aadd2af9e2e5
anand-supernova <anand.tripathy@supernova.xyz>
2018-07-25 11:56:04 +0530

    Changes for prior notify condition

--- app/models/spree/subscription.rb
+++ app/models/spree/subscription.rb
@@ -34,7 +34,7 @@ module Spree
     scope :active, -> { where(enabled: true) }
     scope :not_cancelled, -> { where(cancelled_at: nil) }
     scope :with_appropriate_delivery_time, -> { where("next_occurrence_at <= :current_date", current_date: Time.current) }
-    scope :with_future_delivery_time, -> { where("next_occurrence_at >= :current_date", current_date: Time.current) }
+    scope :with_future_delivery_time, -> { where("DATE(next_occurrence_at) >= ?", Date.today) }
     scope :eligible_for_subscription, -> { unpaused.active.not_cancelled.with_appropriate_delivery_time }
     scope :eligible_for_prior_notification, -> { unpaused.active.not_cancelled.with_future_delivery_time }
 
